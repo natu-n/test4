106375a19ae0d6b782b8e6ad7903ef2f0bea46c0
NORIYUKI NATSUI <natu0819@gmail.com>
2022-03-03 08:40:09 +0900

    2022.03.3

--- src/App.vue
+++ src/App.vue
@@ -15,27 +15,23 @@
         <v-row>
           <v-col cols="2">
             <v-sheet rounded="lg">
-              <v-list color="transparent">
-                <v-list-item v-for="n in 5" :key="n" link>
-                  <v-list-item-content>
-                    <v-list-item-title> List Item {{ n }} </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item link color="grey lighten-4">
-                  <v-list-item-content>
-                    <v-list-item-title> Refresh </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
+              <v-list>
+                <v-list-item-group v-model="model">
+                  <v-list-item v-for="(period, i) in periods" :key="i">
+                    <v-list-item-content>
+                      <v-list-item-title
+                        v-text="period"
+                      ></v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
               </v-list>
             </v-sheet>
           </v-col>
 
           <v-col>
             <v-sheet min-height="70vh" rounded="lg">
-              <router-view class="chart-container"/>
+              <router-view class="chart-container" />
             </v-sheet>
           </v-col>
         </v-row>
@@ -52,6 +48,14 @@ export default Vue.extend({
 
   data: () => ({
     links: ["Calendar", "DataGrid", "LineChart", "Updates"],
+    periods: [
+      "last week",
+      "one month ago",
+      "3 months ago",
+      "6 months ago",
+      "last year",
+      "3 years ago",
+    ],
   }),
 });
 </script>
