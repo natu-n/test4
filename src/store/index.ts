18b717b6b0255bcbc4f92b4b26f80fee4938c5e8
NORIYUKI NATSUI <natu0819@gmail.com>
2022-03-20 17:32:04 +0900

    filter empty data

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,12 @@ import dayjs from "dayjs";
 import axios from "axios";
 
 const API_URL =
-"https://script.google.com/macros/s/AKfycbxIjB1PffnSn96gRvsCQWW2tYAwYZsceQ2_bU-Et0MQWwKxYdqnhV0pCQp0HzFIrI9F8g/exec";
+  "https://script.google.com/macros/s/AKfycbxIjB1PffnSn96gRvsCQWW2tYAwYZsceQ2_bU-Et0MQWwKxYdqnhV0pCQp0HzFIrI9F8g/exec";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
+  state: {
     today: dayjs(new Date()).format("YYYY-MM-DD"),
     toDate: dayjs(new Date()).format("YYYY-MM-DD"),
     info: [],
@@ -17,7 +17,7 @@ export default new Vuex.Store({
   },
   getters: {
     today: (state) => state.today,
-    info: (state) => state.info,
+    info: (state) => state.info.filter(item => item.date != ""),
     isLoaded: (state) => state.isLoaded,
   },
   mutations: {
@@ -36,19 +36,19 @@ export default new Vuex.Store({
   },
   actions: {
     async getJSON(): Promise<void> {
-      console.info("getJSON:"+ this.state.isLoaded);
+      console.info("getJSON:" + this.state.isLoaded);
       if (!this.state.isLoaded) {
         await axios
           .get(API_URL)
           .then((response) => (this.state.info = response.data))
           .finally(() => {
             return (
-              this.state.isLoaded = true,
+              (this.state.isLoaded = true),
               this.commit("setInfo", this.state.info),
               this.commit("setIsLoaded", this.state.isLoaded)
-            )
-          })
+            );
+          });
       }
     },
-  }
-})
+  },
+});
