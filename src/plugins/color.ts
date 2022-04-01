379dcb7962e9637889c570f0d78b9c3dd9fc6946
NORIYUKI NATSUI <natu0819@gmail.com>
2022-04-01 18:20:17 +0900

    共通定数化

---
+++ src/plugins/color.ts
@@ -0,0 +1,12 @@
+// 収縮期血圧閾値
+const _SYSTOLIC_THRESHOLD: number[] = [134, 129, 0];
+// 拡張期血圧閾値
+const _DIASTOLIC_THRESHOLD: number[] = [84, 79, 0];
+// 閾値マーカ
+const _COLORS: string[] = ["red", "orange", "green"];
+
+export default {
+  SYSTOLIC_THRESHOLD: _SYSTOLIC_THRESHOLD,
+  DIASTOLIC_THRESHOLD: _DIASTOLIC_THRESHOLD,
+  COLORS: _COLORS,
+};
